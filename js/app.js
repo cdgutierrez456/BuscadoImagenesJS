aef37f4c5b5ab73afd3441a78de1b515b9cc8376
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-13 23:13:31 -0500

    Mostrando resultados de busqueda en html

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ function validarFormulario(e) {
 
     const terminoBusqueda = document.querySelector('#termino').value;
 
-    if(terminoBusqueda === '') {
+    if (terminoBusqueda === '') {
         mostrarAlerta('Agrega un termino de busqueda');
         return;
     }
@@ -21,17 +21,17 @@ function validarFormulario(e) {
 function mostrarAlerta(mensaje) {
 
     const alerta = document.querySelector('.bg-red-100');
-    if(!alerta) {
+    if (!alerta) {
         const alerta = document.createElement('P');
         alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
-    
+
         alerta.innerHTML = `
             <strong class="font-bold">¡Error!</strong>
             <span class="block sm:inline">${mensaje}</span>
         `;
-    
+
         formulario.appendChild(alerta);
-    
+
         setTimeout(() => {
             alerta.remove();
         }, 2000);
@@ -42,7 +42,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = '19963715-4f70db65d59908dc7a1a2b103';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -52,5 +52,30 @@ function buscarImagenes(termino) {
 }
 
 function mostrarImagenes(imagenes) {
-    console.log(imagenes);
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterando sobre el arreglo de imagenes
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class='bg-white'>
+                    <img class="w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes} <span class="font-light"> Likes </span> </p>
+                        <p class="font-bold"> ${views} <span class="font-light"> Views </span> </p>
+                        <a 
+                            class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
+                            Ver Imagen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+    })
 }
